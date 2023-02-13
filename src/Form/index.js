bcb1f91d5562dbc954e5a1ba5687ec6abcf9bbd7
Bartosz Barc <bartosz.barc721@gmail.com>
2023-02-13 15:27:11 +0100

    Added focus to input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button, Input, StyledForm } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTask] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -10,14 +11,17 @@ const Form = ({ addNewTask }) => {
             addNewTask(newTaskContent.trim());
         }
         setNewTask("");
+        inputRef.current.focus();
     };
 
     return (
         <StyledForm onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 name="task" placeholder="Co jest do zrobienia?"
                 onChange={(event) => setNewTask(event.target.value)}
+                autoFocus
             />
             <Button>Dodaj zadanie</Button>
         </StyledForm>
