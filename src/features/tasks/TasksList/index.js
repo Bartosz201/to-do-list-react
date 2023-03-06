4cbfc70697f60fe477f9f4b13194ab570192f860
Bartosz Barc <bartosz.barc721@gmail.com>
2023-03-06 12:31:16 +0100

    querySearchParameter in TasksList

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import { querySearchParameter } from "../querySearchParameter";
 import { removeTask, selectHideDone, selectTaskByQuery, toggleTaskDone } from "../taskSlice";
 import { Button, Content, Item, List, StyledLink } from "./styled";
 
 const TasksList = () => {
     const location = useLocation()
     const searchParams = new URLSearchParams(location.search)
-    const query = searchParams.get("szukaj")
+    const query = searchParams.get(querySearchParameter)
 
     const tasks = useSelector(state => selectTaskByQuery(state, query))
     const hideDone = useSelector(selectHideDone)
